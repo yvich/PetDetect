12d106e41ef6887d303940c232e452b086910017
Emily <44253515+dange2@users.noreply.github.com>
2022-11-02 10:01:01 -0400

    Login UI Files

---
+++ LoginUI/PetDetect_Login/app/src/main/java/com/example/petdetect_login/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.petdetect_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,LoginFragment())
+            .commit()
+    }
+}
