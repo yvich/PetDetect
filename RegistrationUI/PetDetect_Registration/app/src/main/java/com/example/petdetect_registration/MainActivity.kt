7a34a2665cb61ec9c5e9310a901efa529f3b4974
Emily <44253515+dange2@users.noreply.github.com>
2022-11-02 10:03:16 -0400

    Add files via upload

---
+++ RegistrationUI/PetDetect_Registration/app/src/main/java/com/example/petdetect_registration/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.petdetect_registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,RegistrationFragment())
+            .commit()
+    }
+}
