11bb9faa6dac4f3cade270bcc38e78b324393374
Emily <44253515+dange2@users.noreply.github.com>
2022-11-08 23:00:02 -0500

    Notification UI
    Test notification until firebase is connected to app

---
+++ PetDetect_History2/app/src/main/java/com/example/petdetect_history/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.petdetect_history;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button notifyBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notifyBtn = findViewById(R.id.notify_btn);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        notifyBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Test Notification" );
+                builder.setContentTitle("My Title");
+                builder.setContentText("Notification sent on xx-xx-xxxx at xx:xx");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                managerCompat.notify(1, builder.build());
+
+            }
+
+        });
+
+    }
+}
