8c31752dd1a9034abce95da95ac53295089be0da
alexaLL2000 <113215257+alexaLL2000@users.noreply.github.com>
2022-11-08 21:29:23 -0500

    Add files via upload

--- LoginUI/PetDetect_Login/app/src/main/java/com/example/petdetect_login/DBHandler.java
+++ LoginUI/PetDetect_Login/app/src/main/java/com/example/petdetect_login/DBHandler.java
@@ -2,8 +2,11 @@ package com.example.petdetect;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 public class DBHandler extends SQLiteOpenHelper{
 
@@ -28,19 +31,57 @@ public class DBHandler extends SQLiteOpenHelper{
             db.execSQL(query);
         }
 
-        public void addNewEmail(String emailAddress, String password) {
-            SQLiteDatabase db = this.getWritableDatabase();
-            ContentValues values = new ContentValues();
-
-            values.put(EMAIL_COL, emailAddress);
-            values.put(PASSWORD_COL, password);
-
-            db.insert(TABLE_NAME, null, values);
-
-            db.close();
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
         }
 
-    public void changePassword(String password) {
+        @Override
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            onUpgrade(db, oldVersion, newVersion);
+        }
+
+
+        //Method to read all the emails
+     /*   public <EmailList> ArrayList<EmailList> readEmails(){
+            SQLiteDatabase db = this.getReadableDatabase();
+            Cursor cursorEmails = db.rawQuery(" SELECT * FROM " + TABLE_NAME, null);
+            ArrayList<EmailList> emailListArrayList = new ArrayList<>();
+
+            if (cursorEmails.moveToFirst()) {
+                do {
+                    emailListArrayList.add(new emailListArrayList(cursorEmails.getString(1),
+                            cursorEmails.getString(4),
+                            cursorEmails.getString(2),
+                            cursorEmails.getString(3)));
+                } while (cursorEmails.moveToNext());
+            }
+            cursorEmails.close();
+            return emailListArrayList;
+
+        } */
+
+        //Method to read
+        public Cursor getData (String email){
+            SQLiteDatabase db = this.getReadableDatabase();
+            Cursor res =  db.rawQuery( "select * from contacts where email="+email+" ", null );
+            return res;
+        }
+
+        public boolean UpdateData(String email, String password){
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+
+            contentValues.put(EMAIL_COL,email);
+            contentValues.put(PASSWORD_COL,password);
+
+            db.update(TABLE_NAME, contentValues, " email = ? ", new String[] {email});
+            return true;
+
+        }
+
+    public void addNewEmail(String emailAddress, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
@@ -52,11 +93,16 @@ public class DBHandler extends SQLiteOpenHelper{
         db.close();
     }
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-            onCreate(db);
-        }
+    public void changePassword(String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(PASSWORD_COL, password);
+
+        db.insert(TABLE_NAME, null, values);
+
+        db.close();
+    }
 
 
 
--- LoginUI/PetDetect_Login/app/src/main/java/com/example/petdetect_login/MainLogin.java
+++ LoginUI/PetDetect_Login/app/src/main/java/com/example/petdetect_login/MainLogin.java
@@ -8,8 +8,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainLogin extends AppCompatActivity{
+import com.example.petdetect.DBHandler;
 
+public class MainLogin extends AppCompatActivity {
+    private EditText emailAdd, pass;
+    private Button signIn, forgotPassword, signUp;
     private DBHandler dbHandler;
 
     @Override
@@ -20,19 +23,35 @@ public class MainLogin extends AppCompatActivity{
         // initializing all our variables.
         /// = findViewById(R.id./);
         /// = findViewById(R.id./);
-       // / = findViewById(R.id./);
-       // / = findViewById(R.id./);
-       // / = findViewById(R.id./);
+        // / = findViewById(R.id./);
+        // / = findViewById(R.id./);
+        // / = findViewById(R.id./);
 
         // creating a new dbhandler class
         // and passing our context to it.
         dbHandler = new DBHandler(MainLogin.this);
 
-        addNewEmail.setOnClickListener(new View.OnClickListener() {
+        signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+              /*  String emailAddress = emailAdd.getText().toString();
+                String password = pass.getText().toString();
 
+                // validating if the text fields are empty or not.
+                if (emailAddress.isEmpty() && password.isEmpty()) {
+                    Toast.makeText(MainLogin.this, "Please enter all the data..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // on below line we are calling a method to add new
+                // course to sqlite data and pass all our values to it.
+                dbHandler.addNewEmail(emailAddress, password);
+
+                // after adding the data we are displaying a toast message.
+                Toast.makeText(MainLogin.this, "Course has been added.", Toast.LENGTH_SHORT).show();
+                emailAdd.setText("");
+                pass.setText(""); */
             }
-        });
+
     }
 }
